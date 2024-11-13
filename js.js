09d2e4bc26b5b13c6abd21c84ca10124968aefc4
Akhila-web <akhilareddycholleti@gmail.com>
2024-11-13 02:26:35 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+//question1
+let str="hello world"
+console.log(str.replace("hello world","my dear friend"));
+
+//question2
+let a="Hungry";
+console.log(a.repeat(10));
+
+//question3
+let sentence="the quick brown fox jumps over the lazy dog.The fox is clever"
+console.log(sentence.lastIndexOf("fox"));
+
+//question4
+let message ="hello world"
+console.log(message.startsWith("hi"))
+
+
+//question5
+let filename="document.pdf";
+console.log(filename.endsWith(".pdf"))
